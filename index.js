2a4def4524b81eb66b209313e8e250c124a47273
alirezaahmadibh <alirezaeeee@gmail.com>
2026-05-07 12:34:19 +0330

    session 1

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const sequelize = require('./database/sequelize-connect');
+const User = require('./models/user.model');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+// GET all users
+app.get('/', async (req, res) => {
+    const users = await User.findAll();
+    res.send(users);
+});
+
+// Create user
+app.get('/create-user', async (req, res) => {
+    const user = await User.create({
+        firstName: "Jane",
+        lastName: "Doe",
+        email: "info@owasp.com",
+        password: "admin123"
+    });
+
+    res.send(user);
+});
+
+// Login
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+
+    console.log({email})
+
+
+    const user = await User.findOne({
+        where: { email }
+    });
+
+    console.log(user)
+
+    if (user) {
+        if (user.password == password) {
+            res.send("Hi to panel");
+        } else {
+            res.send("The password is incorrect!");
+        }
+    } else {
+        res.send("User not found!"); 
+    }
+});
+
+// Start server
+app.listen(port, async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync({ force: true });
+
+        console.log('Connection has been established successfully.');
+        console.log(`Example app listening on port ${port}`);
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+});
